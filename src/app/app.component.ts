85c11158cb5b99453cb69dae700247b207cec248
Angel Montoya <joseangelmontoya69@gmail.com>
2024-12-17 19:52:14 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SaludoComponent } from "./saludo/saludo.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SaludoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
